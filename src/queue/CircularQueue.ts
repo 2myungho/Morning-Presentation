23cc32b03847e7b1473bb22684efa079fd3790fd
2myungho <dl_audgh@naver.com>
2021-05-06 19:11:49 +0900

    id

--- src/queue/CircularQueue.ts
+++ src/queue/CircularQueue.ts
@@ -43,7 +43,7 @@ queue.enqueue(2);
 queue.enqueue(3);
 queue.enqueue(4);
 console.log(queue.dequeue());
-// console.log(queue.dequeue());
+console.log(queue.dequeue());
 // queue.enqueue(4);
 // console.log(queue.dequeue());
 // console.log(queue.dequeue());
